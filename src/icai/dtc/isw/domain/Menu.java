b410183f39e56ca21c6d1041a59d72e82088f9fa
MrFat1 <97615672+MrFat1@users.noreply.github.com>
2022-11-06 18:31:28 +0100

    Añadido el icono a la pestaña de búsqueda

--- src/icai/dtc/isw/domain/Menu.java
+++ src/icai/dtc/isw/domain/Menu.java
@@ -23,7 +23,8 @@ public class Menu extends JFrame {
        	Icon conoP = new ImageIcon((new ImageIcon(("resources/icon-profile.png")).getImage()).getScaledInstance(20,20,Image.SCALE_SMOOTH));
         Icon conoC = new ImageIcon((new ImageIcon(("resources/img-chat.png")).getImage()).getScaledInstance(20,20,Image.SCALE_SMOOTH));
         Icon conoV = new ImageIcon((new ImageIcon(("resources/img-video.png")).getImage()).getScaledInstance(30,20,Image.SCALE_SMOOTH));
-        pestanas.addTab("Buscador",new JVentana());
+        Icon conoB = new ImageIcon((new ImageIcon(("resources/icon-busqueda.png")).getImage()).getScaledInstance(30,20,Image.SCALE_SMOOTH));
+        pestanas.addTab("Buscador",conoB,new JVentana());
         pestanas.addTab("Videos",conoV,new PanelVideo());
         pestanas.addTab("Perfil",conoP,new JPanel()); //Proximamente
         pestanas.addTab("Chat",conoC,new JPanel()); //Proximamente
